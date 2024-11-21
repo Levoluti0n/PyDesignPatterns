4a4097448c5277017e7ac417fdc1d0c7d2ad1a7c
Levoluti0n <levon.mezhlumyan.h@gmail.com>
2024-11-21 14:31:36 +0400

    Add Adapter Pattern

---
+++ src/patterns/structural/adapter.py
@@ -0,0 +1,36 @@
+"""
+-------------------------------------------------
+File: adapter.py
+Intent: 
+    Adapter is a structural design pattern that allows
+    objects with incompatible interfaces to collaborate.
+
+Usage:
+    Use the Adapter class when you want to use some existing class,
+    but its interface isn't compatible with the rest of your code.
+-------------------------------------------------
+"""
+
+from abc import ABC, abstractmethod
+
+class PaymentProcessor(ABC):
+    @abstractmethod
+    def pay(self, amount):
+        pass
+
+class StripePayment:
+    def make_payment(self, amount):
+        print(f"Processing payment of ${amount} via Stripe.")
+
+class StripeAdapter(PaymentProcessor):
+    def __init__(self, stripe):
+        self.stripe = stripe
+    
+    def pay(self, amount):
+        self.stripe.make_payment(amount)
+
+def process_payment(amount):
+    payment_processor = StripePayment()
+    stripe_adapter = StripeAdapter(payment_processor)
+    print("Stripe Adapter Created!")
+    stripe_adapter.pay(amount)
